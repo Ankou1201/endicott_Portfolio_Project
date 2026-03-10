675e7ab69bf9c838fa1248eeb219ac4c53ac5456
Ankou1201 <shuyonghui912@gmail.com>
2026-03-10 16:46:49 -0400

    Commit Add method/Doc

--- src/PaletteKernel.java
+++ src/PaletteKernel.java
@@ -1,5 +1,22 @@
 import components.standard.Standard;
 
 public interface PaletteKernel {
-
+ /**
+     * Adds {@code hexColor} to this palette.
+     *
+     * @param hexColor
+     *            the hex color string to add
+     * @aliases reference {@code hexColor}
+     * @updates this
+     * @requires
+     *  <pre>
+     *  hexColor starts with '#' and |hexColor| = 7 and
+     *  hexColor[1..6] are all valid hex characters [0-9A-Fa-f]
+     *  </pre>
+     * @ensures
+     *  <pre>
+     *  this = #this union {hexColor.toUpperCase()}
+     *  </pre>
+     */
+    void add(String hexColor);
 }
